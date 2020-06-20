9137d49a792520e05ff7e9555e17a92694b5994c
Alexej Agadshanjan <stu204977@mail.uni-kiel.de>
2020-06-20 17:55:17 +0200

    tmw cleaning up and setting ids

--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
 require('file-loader?name=[name].[ext]!./index.html');
-import './css/style.css'
-import Logo from './images/logo.svg';
+import './assets/css/animate.css';
+import './assets/css/lineicons.css';
+import './assets/css/bootstrap.min.css';
+import './assets/css/default.css';
+import './assets/css/style.css';
+import './assets/css/gametab.css';
+import Logo from './assets/images/logo.svg';
+import Background from './assets/images/background.png';
+import Download_Shape from './assets/images/download-shape.svg';
+import Email from './assets/images/email.svg';
+import Footer_Shape from './assets/images/footer-shape.svg';
+import Logo_swanz from './assets/images/logo_swanz.svg';
+
+import 'bootstrap';
+window.$ = window.jQuery = require('jquery');
+var WOW = require('wowjs');
 
 const io = require("socket.io-client");
 var socket = io('http://localhost:3030');
-
 
 var defaultStyle = {
   color: 'blue'
@@ -19,25 +32,24 @@ socket.on('connect', () => {
 });
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  var stylingSection = document.getElementById('styling');
-  var createGameSection = document.getElementById('createGame');
+  var createGameSection = document.getElementById('startup');
   var lobbySection = document.getElementById('lobby');
+  var preLobbySection = document.getElementById('preLobby');
+  var gameSettingSection = document.getElementById('gameSettings');
   var joinGameSection = document.getElementById('joinLobby');
 
   document.getElementById('logo').src = Logo;
-  document.getElementById('createGameButton').onclick = function() {
+  document.getElementById('footer-shape').src = Footer_Shape;
+  document.getElementById('footer-shape-1').src = Footer_Shape;
+  document.getElementById('createlobby').onclick = function() {
     socket.emit('createNewGame', {username: username, style: style});
   }
-  document.getElementById('joinGameButton').onclick = function() {
+  document.getElementById('joinlobby').onclick = function() {
     var requestedLobbyUrl = window.location.pathname.replace("/", "").replace("?", "");
     socket.emit('join', {username: username, style: style, requestedLobbyUrl: requestedLobbyUrl});
   }
-  document.getElementById('username').addEventListener('input', function (evt) {
+  document.getElementById('nick').addEventListener('input', function (evt) {
     username = this.value || defaultUsername;
-  });
-
-  document.getElementById('color').addEventListener('input', function (evt) {
-    style.color = this.value || defaultStyle.color;
   });
 
   showHomeStage();
@@ -52,7 +64,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
   }
 
   function showHomeStage() {
-    showSection(stylingSection, true);
     showSection(createGameSection, true);
     showSection(lobbySection, false);
     showSection(joinGameSection, false);
@@ -64,7 +75,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
   }
 
   function showLobbyStage() {
-    showSection(stylingSection, false);
     showSection(createGameSection, false);
     showSection(joinGameSection, false);
     showSection(lobbySection, true);
@@ -103,5 +113,133 @@ document.addEventListener("DOMContentLoaded", function(event) {
   socket.on('reloadPage', function() {
     location.reload();
   });
+
+});
+
+
+$(function () {
+
+    "use strict";
+
+    //===== Prealoder
+
+    $(window).on('load', function (event) {
+        $('.preloader').delay(500).fadeOut(500);
+    });
+
+
+
+    //===== Sticky
+
+    $(window).on('scroll', function (event) {
+        var scroll = $(window).scrollTop();
+        if (scroll < 20) {
+            $(".navbar-area").removeClass("sticky");
+        } else {
+            $(".navbar-area").addClass("sticky");
+        }
+    });
+
+
+
+    //===== Section Menu Active
+
+    var scrollLink = $('.page-scroll');
+    // Active link switching
+    $(window).scroll(function () {
+        var scrollbarLocation = $(this).scrollTop();
+
+        scrollLink.each(function () {
+
+            var sectionOffset = $(this.hash).offset().top - 73;
+
+            if (sectionOffset <= scrollbarLocation) {
+                $(this).parent().addClass('active');
+                $(this).parent().siblings().removeClass('active');
+            }
+        });
+    });
+
+
+    //===== close navbar-collapse when a  clicked
+
+    $(".navbar-nav a").on('click', function () {
+        $(".navbar-collapse").removeClass("show");
+    });
+
+    $(".navbar-toggler").on('click', function () {
+        $(this).toggleClass("active");
+    });
+
+    $(".navbar-nav a").on('click', function () {
+        $(".navbar-toggler").removeClass('active');
+    });
+
+
+
+
+    //===== Back to top
+
+    // Show or hide the sticky footer button
+    $(window).on('scroll', function (event) {
+        if ($(this).scrollTop() > 600) {
+            $('.back-to-top').fadeIn(200)
+        } else {
+            $('.back-to-top').fadeOut(200)
+        }
+    });
+
+
+    //Animate the scroll to yop
+    $('.back-to-top').on('click', function (event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: 0,
+        }, 1500);
+    });
+
+
+    //===== Svg
+
+    jQuery('img.svg').each(function () {
+        var $img = jQuery(this);
+        var imgID = $img.attr('id');
+        var imgClass = $img.attr('class');
+        var imgURL = $img.attr('src');
+
+        jQuery.get(imgURL, function (data) {
+            // Get the SVG tag, ignore the rest
+            var $svg = jQuery(data).find('svg');
+
+            // Add replaced image's ID to the new SVG
+            if (typeof imgID !== 'undefined') {
+                $svg = $svg.attr('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if (typeof imgClass !== 'undefined') {
+                $svg = $svg.attr('class', imgClass + ' replaced-svg');
+            }
+
+            // Remove any invalid XML tags as per http://validator.w3.org
+            $svg = $svg.removeAttr('xmlns:a');
+
+            // Replace image with new SVG
+            $img.replaceWith($svg);
+
+        }, 'xml');
+
+    });
+
+
+    //=====  WOW active
+
+    var wow = new WOW.WOW();
+    wow.init();
+    wow.sync();
+
+
+
+
 
 });
